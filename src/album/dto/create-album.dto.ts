a9b71c3233b816ecc1ddf2ae5553fac0bf86257a
kublashviligiorgi <giorgikublashvili96@gmail.com>
2024-07-21 18:18:32 +0400

    create Album Dto

--- src/album/dto/create-album.dto.ts
+++ src/album/dto/create-album.dto.ts
@@ -1,1 +1,18 @@
-export class CreateAlbumDto {}
+import { Type } from "class-transformer";
+import { IsArray, IsNumber, IsObject, IsString, ValidateNested } from "class-validator"
+
+export class CreateAlbumDto {
+    @IsString()
+    title: string;
+
+    @IsString()
+    releaseDate: string;
+
+    @IsObject()
+    @ValidateNested()
+    @Type(() => CreateMusicDto)
+    musics: CreateMusicDto[];
+
+    @IsNumber()
+    artistId: number
+}
