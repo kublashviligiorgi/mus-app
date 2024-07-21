610a9b929fc092951a33aa56f1f50dc324ae9a02
kublashviligiorgi <giorgikublashvili96@gmail.com>
2024-07-21 17:50:19 +0400

    feat/Jobia-21/create Album Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './album/album.module';
 
 @Module({
-  imports: [],
+  imports: [AlbumModule],
   controllers: [AppController],
   providers: [AppService],
 })
