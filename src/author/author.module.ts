4a0052a3af0632f97fff871560291ff3335b1e92
temoSephe <seferteladze21@gmail.com>
2024-07-22 16:26:47 +0400

    Update author.module.ts

--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthorService } from './author.service';
 import { AuthorController } from './author.controller';
+import { AuthorRepository } from './author.repository';
 
 @Module({
   controllers: [AuthorController],
-  providers: [AuthorService],
+  providers: [AuthorService,AuthorRepository],
 })
 export class AuthorModule {}
