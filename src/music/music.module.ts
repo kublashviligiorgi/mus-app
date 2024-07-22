04f65c03a325dd1ba914dda5908535385a51fd37
temoSephe <seferteladze21@gmail.com>
2024-07-22 15:34:56 +0400

    prepare music

--- src/music/music.module.ts
+++ src/music/music.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MusicService } from './music.service';
 import { MusicController } from './music.controller';
+import { MusicRepository } from './music.repository';
 
 @Module({
   controllers: [MusicController],
-  providers: [MusicService],
+  providers: [MusicService,MusicRepository],
 })
 export class MusicModule {}
