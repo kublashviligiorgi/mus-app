0b16fb2c42f745e3156a8fc250072ad65c8745e2
temoSephe <seferteladze21@gmail.com>
2024-07-22 16:25:28 +0400

    create author module

---
+++ src/author/author.repository.ts
@@ -0,0 +1,49 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { AuthorEntity } from "./entities/author.entity";
+import { Repository } from "typeorm";
+import { CreateAuthorDto } from "./dto/create-author.dto";
+import { UpdateAuthorDto } from "./dto/update-author.dto";
+
+@Injectable()
+export class AuthorRepository {
+    constructor(
+        @InjectRepository(AuthorEntity)
+        private authorRepository: Repository<AuthorEntity>
+    ) { }
+
+    create(data: CreateAuthorDto) {
+        const newAuthor = new AuthorEntity()
+
+        newAuthor.firstName = data.firstName;
+        newAuthor.lastName = data.lastName;
+        newAuthor.biographi = data.biographi
+
+        return this.authorRepository.save(newAuthor)
+    }
+
+    findAll() {
+        return this.authorRepository.find()
+    }
+
+    findOne(id: number) {
+        return this.authorRepository
+            .createQueryBuilder('author')
+            .where('athor.id = :id', { id })
+            .getOne()
+    }
+
+    update(id: number, data: UpdateAuthorDto) {
+        const updatedAuthor = new AuthorEntity()
+        updatedAuthor.id = id;
+        updatedAuthor.firstName = data.firstName;
+        updatedAuthor.lastName = data.lastName;
+        updatedAuthor.biographi = data.biographi;
+
+        return this.authorRepository.update(id, updatedAuthor)
+    }
+
+    remove(id: number) {
+        return this.authorRepository.softDelete({ id })
+    }
+}
---
+++ src/author/entities/author.entity.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class AuthorEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'string', length: 100 })
+    firstName: string;
+
+    @Column({ type: 'string', length: 100 })
+    lastName: string;
+
+    @Column({type:'longtext'})
+    biographi: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
